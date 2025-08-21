96ab755c5522f9c88bf2965ed9b454af0d2863c8
utsav102005 <sojitrautsava@gmail.com>
2025-08-21 15:14:18 +0530

    cmmit

---
+++ main.js/script.js
@@ -0,0 +1,7 @@
+ <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+ var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
